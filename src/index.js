a9b6145909f39255d339f249147bb4f331af3f6c
unknown <skopin.n@gmail.com>
2019-12-24 23:14:08 +0700

    add cors morgan body-parser and setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import express from 'express'
 import path from 'path'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({errors: {
-        global: "Invalid credentials"
-    }})
+app.post('/api/register', (req, res) => {
+    res.send({
+        message: 'You were registered.'
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    return res.status(403).json({
+        error: 'The login information was incorrect'
+    })
 })
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(8081, () => console.log('running on localhost:8081'))
+app.listen(process.env.PORT || 8081, () => console.log('running on localhost:8081'))
