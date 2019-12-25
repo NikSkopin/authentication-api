2a922fd0f5b2afe137f17b97d962125e590628bc
unknown <skopin.n@gmail.com>
2019-12-25 15:56:14 +0700

    eslint setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import express from 'express'
-import path from 'path'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
-const app = express()
-app.use(morgan('combine'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/api/register', (req, res) => {
-    res.send({
-        message: 'You were registered.'
-    })
-})
+  res.send({
+    message: 'You were registered.',
+  });
+});
 
-app.post('/api/login', (req, res) => {
-    return res.status(403).json({
-        error: 'The login information was incorrect'
-    })
-})
+app.post('/api/login', (req, res) => res.status(403).json({
+  error: 'The login information was incorrect',
+}));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(process.env.PORT || 8081, () => console.log('running on localhost:8081'))
+app.listen(process.env.PORT || 8081, () => console.log('running on localhost:8081'));
