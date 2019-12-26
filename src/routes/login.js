fac63bc06ed261536695fa5d06ab0089ef21c56b
unknown <skopin.n@gmail.com>
2019-12-26 22:52:24 +0700

    login verifications

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,13 +5,27 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    console.log('here');
-
     const credentials = req.body;
     const user = await User.findOne({ email: credentials.email });
-    if (user) {
-      res.json({ success: true });
+    if (!user) {
+      return res.status(403).send({
+        error: 'The login information was incorrect.',
+      });
     }
+
+    const isPasswordValid = await user.validPassword(credentials.password);
+    if (!isPasswordValid) {
+      return res.status(403).send({
+        error: 'The password information was incorrect.'
+      });
+    }
+
+    const userJson = user.toAuthJSON();
+    res.send({
+      email: userJson.email,
+      token: userJson.token,
+    });
+
   } catch (error) {
     res.status(403).json({
       error: 'The login information was incorrect',
